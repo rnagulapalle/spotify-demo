7561ea3c2c91f9d4ad992b98c87305c9357fe856
MBS <support@allmydibs.com>
2018-06-05 16:44:22 +0530

    removed dummy data

--- src/components/Artists.js
+++ src/components/Artists.js
@@ -7,47 +7,6 @@ import {
   setTokens,
 } from '../actions/actions';
 
-//Temporarily store data here
-const PostsData = [
-  {
-    "category": "News",
-    "title": "CNN Acquire BEME",
-    "text": "CNN purchased Casey Neistat's Beme app for $25million.",
-    "image": "https://source.unsplash.com/user/erondu/600x400"
-  },
-  {
-    "category": "Travel",
-    "title": "Nomad Lifestyle",
-    "text": "Learn our tips and tricks on living a nomadic lifestyle",
-    "image": "https://source.unsplash.com/user/_vickyreyes/600x400"
-  },
-  {
-    "category": "Development",
-    "title": "React and the WP-API",
-    "text": "The first ever decoupled starter theme for React & the WP-API",
-    "image": "https://source.unsplash.com/user/ilyapavlov/600x400"
-  },
-  {
-    "category": "News",
-    "title": "CNN Acquire BEME",
-    "text": "CNN purchased Casey Neistat's Beme app for $25million.",
-    "image": "https://source.unsplash.com/user/erondu/600x400"
-  },
-  {
-    "category": "Travel",
-    "title": "Nomad Lifestyle",
-    "text": "Learn our tips and tricks on living a nomadic lifestyle",
-    "image": "https://source.unsplash.com/user/_vickyreyes/600x400"
-  },
-  {
-    "category": "Development",
-    "title": "React and the WP-API",
-    "text": "The first ever decoupled starter theme for React & the WP-API",
-    "image": "https://source.unsplash.com/user/ilyapavlov/600x400"
-  }
-]
-
-
 class Artist extends React.Component{
   render() {
     return (
@@ -74,15 +33,6 @@ class Artists extends Component {
 
    constructor() {
     super();
-    
-    this.state = {
-      posts: {}
-    }
-  }
-  componentWillMount() {
-    this.setState({
-      posts: PostsData
-    });
   }
 
   render() {

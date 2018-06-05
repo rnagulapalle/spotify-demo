2f85f1c165a36e35f87385f5f05fa3c919e775d8
MBS <support@allmydibs.com>
2018-06-05 12:01:05 +0530

    Added the card into artist

--- src/components/Artists.js
+++ src/components/Artists.js
@@ -1,10 +1,52 @@
 import React, { Component } from 'react';
 import { connect }      from 'react-redux';
 import InfiniteGrid from 'react-infinite-grid';
+
 import {
   getArtists,
   setTokens,
 } from '../actions/actions';
+
+//Temporarily store data here
+const PostsData = [
+  {
+    "category": "News",
+    "title": "CNN Acquire BEME",
+    "text": "CNN purchased Casey Neistat's Beme app for $25million.",
+    "image": "https://source.unsplash.com/user/erondu/600x400"
+  },
+  {
+    "category": "Travel",
+    "title": "Nomad Lifestyle",
+    "text": "Learn our tips and tricks on living a nomadic lifestyle",
+    "image": "https://source.unsplash.com/user/_vickyreyes/600x400"
+  },
+  {
+    "category": "Development",
+    "title": "React and the WP-API",
+    "text": "The first ever decoupled starter theme for React & the WP-API",
+    "image": "https://source.unsplash.com/user/ilyapavlov/600x400"
+  },
+  {
+    "category": "News",
+    "title": "CNN Acquire BEME",
+    "text": "CNN purchased Casey Neistat's Beme app for $25million.",
+    "image": "https://source.unsplash.com/user/erondu/600x400"
+  },
+  {
+    "category": "Travel",
+    "title": "Nomad Lifestyle",
+    "text": "Learn our tips and tricks on living a nomadic lifestyle",
+    "image": "https://source.unsplash.com/user/_vickyreyes/600x400"
+  },
+  {
+    "category": "Development",
+    "title": "React and the WP-API",
+    "text": "The first ever decoupled starter theme for React & the WP-API",
+    "image": "https://source.unsplash.com/user/ilyapavlov/600x400"
+  }
+]
+
 
 class Artist extends React.Component{
   render() {
@@ -29,11 +71,27 @@ class Artists extends Component {
     dispatch(setTokens({ accessToken, refreshToken }));
     dispatch(getArtists());
   }
+
+   constructor() {
+    super();
+    
+    this.state = {
+      posts: {}
+    }
+  }
+  componentWillMount() {
+    this.setState({
+      posts: PostsData
+    });
+  }
+
+
   render() {
     // injected via react-router
     console.log(this.props);
     const { accessToken, refreshToken, artists } = this.props;
     console.log('artists inside component', artists);
+    
     console.log(artists.loading);
     if (artists.loading) {
       console.log('yes');
@@ -42,6 +100,7 @@ class Artists extends Component {
     if (!artists.artists) {
       return <h2>Something went wrong while fetching artist</h2>;
     }
+
     // var items = [];
     // for (var obj in artists.artists) {
     //   items.push(<Artist image={obj.images[0].url} />);
@@ -49,11 +108,92 @@ class Artists extends Component {
 
     // return (<InfiniteGrid wrapperHeight={400} entries={items} />, document.getElementById('grid'));
     return (
-      <div className="msg">
-        <h2>This page is under construction!</h2>
-      </div>
+      <div>
+      <header className="app-header"></header>
+      <Title />
+      <div className="app-card-list" id="app-card-list">
+        {
+          Object
+          .keys(this.state.posts)
+          .map(key => <Card key={key} index={key} details={this.state.posts[key]}/>)
+        }
+    </div>
+    </div>
     );
   }
 }
 
 export default connect(state => state)(Artists);
+
+
+
+
+class Title extends React.Component {
+  render() {
+    return <section className="app-title">
+      <div className="app-title-content">
+        <h1>Top Artist</h1>
+        <p>Covering Spotify Artist</p>
+        <a className="designer-link" href="https://dribbble.com/shots/1978243-Latest-News" target="_blank">Design from <i className="fa fa-dribbble"></i></a>
+      </div>
+    </section>
+  }
+}
+
+
+class Button extends React.Component {
+  render() {
+    return (
+      <button className="button button-primary">
+        <i className="fa fa-chevron-right"></i> Find out more
+      </button>
+    )
+  }
+}
+
+
+class CardHeader extends React.Component {
+  render() {
+    const { image, category } = this.props;
+    var style = { 
+        backgroundImage: 'url(' + image + ')',
+    };
+    return (
+      <header style={style} className="card-header">
+        <h4 className="card-header--title">{category}</h4>
+      </header>
+    )
+  }
+}
+
+
+class CardBody extends React.Component {
+  render() {
+    return (
+      <div className="card-body">
+        <p className="date">March 20 2015</p>
+        
+        <h2>{this.props.title}</h2>
+        
+        <p className="body-content">{this.props.text}</p>
+        
+        <Button />
+      </div>
+    )
+  }
+}
+
+
+class Card extends React.Component {
+  render() {
+    return (
+      <article className="card">
+        <CardHeader category={this.props.details.category} image={this.props.details.image}/>
+        <CardBody title={this.props.details.title} text={this.props.details.text}/>
+      </article>
+    )
+  }
+}
+
+
+
